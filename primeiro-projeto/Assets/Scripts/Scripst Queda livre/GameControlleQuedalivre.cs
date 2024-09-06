e0244858cc77e69b9231fe792b797330f8468f48
unknown <rafael.augustoprof@gmail.com>
2024-09-06 16:14:38 -0300

    Atualizacao QuedaLivre

--- primeiro-projeto/Assets/Scripts/Scripst Queda livre/GameControlleQuedalivre.cs
+++ primeiro-projeto/Assets/Scripts/Scripst Queda livre/GameControlleQuedalivre.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements.Experimental;
 
 public class GameControlleQuedalivre : MonoBehaviour
 {
@@ -12,14 +15,23 @@ public class GameControlleQuedalivre : MonoBehaviour
     public Vector3 posicaoBola;
     public float velocidadeBola;
     public float aceleracaoBola;
+    public GameObject textoG;
+    public GameObject textoV;
+    public GameObject textoK;
+    public GameObject textoU;
 
     // Start is called before the first frame update
     void Awake()
     {
         Time.timeScale = 0f;
 
-        if(objetoQuedaLivre)
-            objetoBola = objetoQuedaLivre.GetComponent<Rigidbody>();      
+        if(objetoQuedaLivre){
+            objetoBola = objetoQuedaLivre.GetComponent<Rigidbody>();
+            textoG = GameObject.Find("Gnumber");
+            textoV = GameObject.Find("Vnumber");
+            textoK = GameObject.Find("ECnumber");
+            textoU = GameObject.Find("EPnumber");
+        }
     }
 
     // Update is called once per frame
@@ -30,8 +42,13 @@ public class GameControlleQuedalivre : MonoBehaviour
             posicaoBola = objetoBola.position;
             velocidadeBola = objetoBola.velocity.magnitude;
             aceleracaoBola = Physics.gravity.y;
+
+            textoG.GetComponent<TextMeshProUGUI>().text = (-1*aceleracaoBola) + " m/s²";
+            textoV.GetComponent<TextMeshProUGUI>().text = Mathf.RoundToInt(velocidadeBola) + " m/s";
+            textoK.GetComponent<TextMeshProUGUI>().text = EnergiaCinetica() + " J";
+            textoU.GetComponent<TextMeshProUGUI>().text = EnergiaPotencial() + " J";
         }
-    }
+    }   
 
     public void StartGame(){
         Time.timeScale = 1f;
@@ -44,5 +61,13 @@ public class GameControlleQuedalivre : MonoBehaviour
     public void ResetGame(){
         UnityEngine.SceneManagement.Scene courrentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(courrentScene.name);
+    }
+
+    private int EnergiaPotencial(){
+        return (Mathf.RoundToInt((-1)*objetoBola.mass*aceleracaoBola*objetoBola.position.y));
+    }
+
+    private int EnergiaCinetica(){
+        return (Mathf.RoundToInt((objetoBola.mass*velocidadeBola*velocidadeBola)/2));
     }
 }
